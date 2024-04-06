a5c272b5bc64e266356c226627b251da07f50f25
Alicia Vento <aliciavento@gmail.com>
2024-04-05 23:51:50 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//importar express
+
+const express = require('express');
+
+//instanciar express
+const app = express();
+
+//levantar el servidor en el puerto 3000
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.use(express.static("assets"));//middelware uso general - carpeta pública
+
+const usuarios=["Juan", "Jocelyn", "Astrid", "Maria", "Ignacia", "Javier", "Brian"];
+
+//Creación de la primera ruta
+app.get("/abracadabra/usuarios", (req, res) => {
+    res.send({usuarios});
+});
+
+//Creación del middleware de la segunda ruta
+app.use("/abracadabra/juego/:usuario", (req, res, next) => {
+    usuarios.includes(req.params.usuario) ? next():res.redirect("/who.jpeg")
+})
+
+app.get("/abracadabra/juego/:usuario", (req, res) => {
+    res.sendFile(_dirname + '/index.html')
+})
+
+//Ruta genérica de que la página no existe
+app.get("*", (req, res) => {
+    res.status(404).send("Página no encontrada  :(");
+});
